1d5edf3abb92f1a353dc343ff37cb19e21a0a15a
David Lopez <dg_lf@hotmail.com>
2021-08-23 21:37:30 -0500

    maquetacion sin css

---
+++ src/TodoCounter.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles/TodoCounter.css"
+
+
+function TodoCounter() {
+    return (
+        <h2 className="TodoCounter"> Has completados 1000 8000 TODOs</h2>
+    )
+}
+
+export default TodoCounter;
