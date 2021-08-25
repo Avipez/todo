c570ecc0376f1e069f579f1919719efe1c8af4fa
David Lopez <dg_lf@hotmail.com>
2021-08-24 19:39:25 -0500

    algo de estilos

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import React from "react";
 const todos = [
   {text: "Cortar cebolla", completed: false},
   {text: "Tomar curso de react", completed: false},
-  {text: "llorar como lloro hernan cortes", completed: false}
+  {text: "Llorar como lloro hernan cortes", completed: false}
 
 ]
 
@@ -19,7 +19,7 @@ function App() {
       <TodoCounter />
       <TodoSearch />
       <TodoList>
-        {todos.map(todo => (<TodoItem key={todo.text} text={todo.text} />))}
+        {todos.map(todo => (<TodoItem key={todo.text} text={todo.text} completed={todo.completed} />))}
       </TodoList>
       {/* <CreateTodoButton /> */}
       <button>+</button>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./styles/TodoItem.css"
 
 function TodoItem(props) {
     return (
-        <li>
-            <span>C</span>
-            <p>{props.text}</p>
-            <span>X</span>
+        <li className="item">
+            <span className="check fas fa-check"></span>
+            <p className="todo-description">{props.text}</p>
+            <span className="delete-todo fas fa-times"></span>
         </li>
     )
 }
