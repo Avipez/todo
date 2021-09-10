7ede87027f64edd5e3def9c6e8de92967b877485
David Lopez <dg_lf@hotmail.com>
2021-09-10 08:21:21 -0500

    Usando React Context para compartir estados

--- src/App/index.js
+++ src/App/index.js
@@ -1,78 +1,13 @@
 import AppUI from "./AppUI";
 import React from "react";
-
-function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
-  
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = [];
-  } else {
-    parsedItem = JSON.parse(localStorageItem)
-  };
-
-  const [item, setItem] = React.useState(parsedItem);
-  
-  const saveItem = (newItem) => {
-    const stringifiedItem = JSON.stringify(newItem);
-    localStorage.setItem(itemName, stringifiedItem);
-    setItem(newItem);
-  }
-
-  return [
-    item,
-    saveItem
-  ];
-}
+import {TodoProvider} from "../TodoContext"
 
 function App() {
-
-  const [todos, saveTodos] = useLocalStorage("TODOS_V1", []);
-
-  const [searchValue, setSearchValue] = React.useState("");
-
-  const completedTodos = todos.filter( todo => !!todo.completed).length;
-  const totalTodos = todos.length;
-
-  let searchedTodos = [];
-
-  if (!searchedTodos >= 1) {
-    searchedTodos = todos;
-  } else {
-    searchedTodos = todos.filter(todo => {
-      const todoText = todo.text = todo.text.toLowerCase();
-      const searchedText = searchValue.toLowerCase();
-      return todoText.includes(searchedText);
-    })
-  }
-
-  const completeTodo = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
-    const newTodos = [...todos];
-    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
-    saveTodos(newTodos);
-  }
-
-  const deleteTodo = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
-    const newTodos = [...todos];
-    newTodos.splice(todoIndex, 1);
-    saveTodos(newTodos);
-  }
-  
   return (
-    <AppUI 
-    totalTodos = {totalTodos}
-    completedTodos = {completedTodos}
-    searchValue = {searchValue}
-    setSearchValue = {setSearchValue}
-    searchedTodos = {searchedTodos}
-    completeTodo={completeTodo}
-    deleteTodo={deleteTodo}
-    />
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
   );
 }
 
 export default App;
-
--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { TodoContext } from "../TodoContext";
 import "./TodoCounter.css"
 
 
-function TodoCounter({completed, total}) {
-
+function TodoCounter() {
+    const { totalTodos, completedTodos } = React.useContext(TodoContext);
     return (
-        <h2 className="TodoCounter"> Has completados {completed} de {total} TODOs</h2>
+        <h2 className="TodoCounter"> Has completados {completedTodos} de {totalTodos} TODOs</h2>
     )
 }
 
