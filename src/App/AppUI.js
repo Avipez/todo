7a32834e15075d938d2f2200750ea5b99299a318
David Lopez <dg_lf@hotmail.com>
2021-12-09 19:31:45 -0500

    update curso patrones de render hasta clase 9

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,37 +1,49 @@
 import React from "react";
-import "./appUI.css"
-import { TodoContext } from "../TodoContext";
+import "./appUI.css";
+import { TodoContext } from "./useTodos";
 import TodoCounter from "../TodoCounter";
 import TodoSearch from "../TodoSearch";
 import TodoList from "../TodoList";
 import TodoItem from "../TodoItem";
 import { CreateTodoButton } from "../CreateTodoButton";
-import { Modal } from "../Modal"
-import { TodoForm } from "../TodoForm"
-import {LoadingSkeleton} from "../LoadingSkeleton"
+import { Modal } from "../Modal";
+import { TodoForm } from "../TodoForm";
+import { LoadingSkeleton } from "../LoadingSkeleton";
 import { WithLove } from "../Footer";
+import Todoheader from "../TodoHeader/Todoheader";
 
 function AppUI() {
-  const { 
+  const {
     error,
     loading,
     searchedTodos,
     completeTodo,
     deleteTodo,
     openModal,
-    setOpenModal
+    setOpenModal,
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
   } = React.useContext(TodoContext);
 
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <Todoheader>
+        <TodoCounter totalTodos={totalTodos} completedTodos={completedTodos} />
+        <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+      </Todoheader>
       <TodoList>
         {error && <p className="UI">Hijole compa, eso no se va poder</p>}
-        {loading && new Array(3).fill(1).map(
-          (item, index) => <LoadingSkeleton key={index} />
+        {loading &&
+          new Array(3)
+            .fill(1)
+            .map((item, index) => <LoadingSkeleton key={index} />)}
+        {!loading && !searchedTodos.length && (
+          <p className="UI">
+            Crea tu primer TODO con el boton <span className="mas">+</span>{" "}
+          </p>
         )}
-        {!loading && !searchedTodos.length && <p className="UI">Crea tu primer TODO con el boton <span className="mas">+</span> </p>}
         {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
@@ -45,15 +57,11 @@ function AppUI() {
 
       {!!openModal && (
         <Modal>
-          <TodoForm>
-          </TodoForm>
+          <TodoForm></TodoForm>
         </Modal>
       )}
 
-      <CreateTodoButton 
-        setOpenModal={setOpenModal}
-        openModal={openModal}
-      />
+      <CreateTodoButton setOpenModal={setOpenModal} openModal={openModal} />
       <WithLove />
     </React.Fragment>
   );
